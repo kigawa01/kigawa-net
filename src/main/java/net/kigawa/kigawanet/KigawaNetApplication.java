98514d4c180950ddc9b0a8a63dcaafab893973c1
kigawa <kigawa.8390@gmail.com>
2023-02-02 01:34:21 +0900

    init

---
+++ src/main/java/net/kigawa/kigawanet/KigawaNetApplication.java
@@ -0,0 +1,15 @@
+package net.kigawa.kigawanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KigawaNetApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(KigawaNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/kigawa/kigawanet/KigawaNetApplicationTests.java
@@ -0,0 +1,15 @@
+package net.kigawa.kigawanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KigawaNetApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
